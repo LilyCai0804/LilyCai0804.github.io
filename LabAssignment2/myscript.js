16f2ebfb254d9cb11fee9fd97f0e8e0efeff3985
LilyCai0804 <caiy065@newschool.edu>
2020-02-27 12:32:43 -0500

    add existing files

---
+++ LabAssignment2/myscript.js
@@ -0,0 +1,30 @@
+function myMoveFunction() {
+  document.getElementById('title').style.letterSpacing = '225px';
+}
+
+
+function myFunction() {
+  document.getElementById("quote").innerHTML = "Before you run out the door, consider two things: the future is already set, only the past can be changed, and if it was worth forgetting, it's not worth remembering";
+}
+
+
+
+
+function myShorterFunction1() {
+  document.getElementById("p1").style.width = "500px";
+}
+
+
+
+function myShorterFunction2() {
+  document.getElementById("p2").style.width= "500px";
+}
+
+function myShorterFunction3() {
+  document.getElementById("p3").style.width = "500px";
+}
+
+
+function myShorterFunction4() {
+  document.getElementById("p4").style.width = "500px";
+}
