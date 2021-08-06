ad091c6035df9bb4a00600e5ba46b0d526d74f56
蔡语沁 <lilycai@caiyuqins-MacBook-Pro.local>
2021-08-06 21:30:50 +0800

    add all

---
+++ where_to_go/main.js
@@ -0,0 +1,163 @@
+const myFunction = () => {
+  document.getElementById("first").style.display ='block';
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction2 = () => {
+  document.getElementById("second").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction3 = () => {
+  document.getElementById("third").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction4 = () => {
+  document.getElementById("forth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction5 = () => {
+  document.getElementById("fifth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction6 = () => {
+  document.getElementById("sixth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+}
+
+const myFunction7 = () => {
+  document.getElementById("seventh").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction8 = () => {
+  document.getElementById("eighth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction9 = () => {
+  document.getElementById("ninth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction10 = () => {
+  document.getElementById("tenth").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("eleventh").style.display ='none'
+
+}
+
+const myFunction11 = () => {
+  document.getElementById("eleventh").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+  document.getElementById("forth").style.display ='none'
+  document.getElementById("fifth").style.display ='none'
+  document.getElementById("sixth").style.display ='none'
+  document.getElementById("seventh").style.display ='none'
+  document.getElementById("eighth").style.display ='none'
+  document.getElementById("ninth").style.display ='none'
+  document.getElementById("tenth").style.display ='none'
+
+}
